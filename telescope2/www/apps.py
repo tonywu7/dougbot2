43537bc04a21893d4845378081c8de83d72a67fc
tonyzbf <tonyzbf@protonmail.com>
2021-06-04 09:26:51 -0400

    Setup web environment

---
+++ telescope2/www/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WWWConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'telescope2.www'
---
+++ telescope2/www/bundle/src/scripts/app/index.js
@@ -0,0 +1,51 @@
+// index.js
+// Copyright (C) 2021  @tonyzbf +https://github.com/tonyzbf/
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const MODULES = ['./main']
+
+requirejs.config({
+    baseUrl: '/static/bundle/scripts/app',
+    paths: {
+        lodash: 'https://cdn.jsdelivr.net/npm/lodash@4.17.20/lodash.min',
+    },
+    deps: ['lodash'],
+    onNodeCreated: (node, config, module, path) => {
+        const SRI = {
+            lodash: 'sha256-ur/YlHMU96MxHEsy3fHGszZHas7NzH4RQlD4tDVvFhw=',
+        }
+        if (SRI[module]) {
+            node.setAttribute('integrity', SRI[module])
+            node.setAttribute('crossorigin', 'anonymous')
+        }
+    },
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    require(MODULES, (...module) => {
+        for (let i = 0, l = module.length; i < l; i++) {
+            module[i].init()
+        }
+    })
+})
+
+window.addEventListener('load', () => {
+    require(MODULES, (...module) => {
+        for (let i = 0, l = module.length; i < l; i++) {
+            let lateInit = module[i].lateInit
+            if (lateInit) lateInit()
+        }
+    })
+})
