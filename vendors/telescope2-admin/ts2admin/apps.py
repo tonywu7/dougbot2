b7b05a2b2de4d408fba32ddb2b288e1c63636349
tonyzbf <tonyzbf@protonmail.com>
2021-08-05 19:54:55 -0400

    Reset subtrees

--- vendors/telescope2-admin/ts2admin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TS2AdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ts2admin'
--- vendors/telescope2-admin/ts2admin/web/bundle/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as changelist from './scripts/changelist'
-
-import './styles/index.scss'
-
-window.addEventListener('DOMContentLoaded', () => {
-    changelist.init()
-})
