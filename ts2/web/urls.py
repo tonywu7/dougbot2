8e6c4ada30ea8648dd30af3fe4247ee5c8e47c90
tonyzbf <tonyzbf@protonmail.com>
2021-10-22 07:49:01 +0800

    Remove .html redirect

--- ts2/web/urls.py
+++ ts2/web/urls.py
@@ -2,13 +2,12 @@ from csp.decorators import csp_exempt
 from django.conf import settings
 from django.urls import include, re_path
 
+from . import schema
 from ._compat.graphql_django import GraphQLView_
-from . import schema, views
 
 app_name = 'web'
 
 urlpatterns = [
-    re_path(r'.*\.html?', views.remove_suffix, name='suffix_redirect'),
     re_path(r'', include('ts2.web.dispatch.public.urls')),
     re_path(r'^gateway/', include('ts2.web.dispatch.gateway.urls')),
     re_path(r'^guild/(?P<guild_id>[0-9]+)/', include('ts2.web.dispatch.manage.urls')),
